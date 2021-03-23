e1c4157fd6b737e1d1ab7e355015707bd4096dc1
zengjunjie <>
2021-03-23 16:33:51 +0800

    rename package

--- activiti/src/main/java/com/example/activiti/controller/TestController.java
+++ activiti/src/main/java/com/example/activiti/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.activiti.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- activiti/src/test/java/com/example/activiti/ActivitiTest.java
+++ activiti/src/test/java/com/example/activiti/ActivitiTest.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.activiti;
 
 import org.junit.jupiter.api.Test;
 
